79fd25605a874ea54edb352c20a295414d0c7439
tinktin <er.ranjansr@gmail.com>
2026-07-04 17:14:26 +0530

    created crud functionalities for category

--- blog_main/views.py
+++ blog_main/views.py
@@ -37,7 +37,7 @@ def register(request):
     context = {
         'form': form
     }
-    return render(request,'register.html')
+    return render(request,'register.html',context)
 
 def login(request):
     if request.method == 'POST':
@@ -49,7 +49,7 @@ def login(request):
             user = auth.authenticate(username=username,password=password)
             if user is not None:
                 auth.login(request,user)
-            return redirect('home')
+            return redirect('dashboard')
     form = AuthenticationForm()
     context = {
         'form':form
@@ -59,3 +59,6 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('home')
+
+
+################## 5 hr 20 min
